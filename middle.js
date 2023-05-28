d9b3acc59671e34d95f064450809b61dcdedd19f
HKARATAS20 <HKARATAS20@KU.EDU.TR>
2023-05-29 00:27:51 +0300

    Add Product modal
    Biraz Chatgpt biraz ben ve bolca sinir.(Product pagein kapama butonunun yerini ayarlayamadım)

--- middle.js
+++ middle.js
@@ -58,63 +58,77 @@ function productsby() {
         cell2.innerHTML = products[i].price;
         cell3.innerHTML = products[i].category;
 
-        var descriptionDiv = document.createElement("div"); // Create a new <div> element for description
         descriptionDiv.style.display = "none"; // Hide the description initially
         row.appendChild(descriptionDiv); // Append the <div> element to the row
 
-        var button = document.createElement("button");
-        button.innerHTML = "Show Description";
-        cell4.appendChild(button);
-        button.addEventListener("click", createShowDescriptionHandler(descriptionDiv, products[i].description));
-  
+        var showButton = document.createElement("button");
+        showButton.innerHTML = "Show Description";
+        showButton.addEventListener("click", createShowDescriptionHandler(products[i]));
+        cell4.appendChild(showButton);
       }
       table.style.display = "block";
-
-
-    }); 
+    });
 }
 
-function createShowDescriptionHandler(descriptionDiv, description) {
+function createShowDescriptionHandler(product) {
   return function() {
-    if (descriptionDiv.style.display === "none") {
-      descriptionDiv.style.display = "block";
-      descriptionDiv.innerHTML = description; // Set the description content
-    } else {
-      descriptionDiv.style.display = "none";
-      descriptionDiv.innerHTML = ""; // Clear the description content
-    }
+    showProductDetails(product);
   };
 }
-function getLifeExpectancy() {
-    var agg = document.getElementById("Operation-select").value;
-    var country = document.getElementById("Country").value;
-  
-    fetch('http://localhost:3000/aggregateCountries?agg=' + agg + '&country=' + country)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      var table = document.getElementById("Countries");
-      for(var i = table.rows.length -1; i > 0;i--){
-        table.deleteRow(i);
-      }
 
-      for(var i = 0; i < data.length; i++){
-        var row = table.insertRow(i+1);
+function showProductDetails(product) {
+  // Create a modal or popup element
+  var modal = document.createElement("div");
+  modal.classList.add("product-modal");
 
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        cell1.innerHTML = data[i].Name;
-        cell2.innerHTML = data[i].LifeExpectancy;
-        cell3.innerHTML = data[i].GovernmentForm;
-        cell4.innerHTML = data[i].Language;
-  
-      }
-      table.style.display = "block";
+  // Create the product details content
+  var content = document.createElement("div");
+  content.classList.add("product-content");
 
+  // Create elements for name, price, and description
+  var nameElement = document.createElement("h2");
+  nameElement.textContent = product.name;
 
-    }); 
-    
+  var priceElement = document.createElement("p");
+  priceElement.textContent = "Price: " + product.price;
+
+  var descriptionElement = document.createElement("p");
+  descriptionElement.textContent = product.description;
+
+// Create a close button
+var closeButton = document.createElement("button");
+closeButton.innerHTML = "X";
+closeButton.classList.add("close-button");
+closeButton.addEventListener("click", function() {
+  closeModal(modal);
+});
+  // Create an "Add to Cart" button
+  var addToCartButton = document.createElement("button");
+  addToCartButton.textContent = "Add to Cart";
+  addToCartButton.addEventListener("click", function() {
+    addToCart(product);
+  });
+
+  // Append elements to the content container
+  content.appendChild(nameElement);
+  content.appendChild(priceElement);
+  content.appendChild(descriptionElement);
+  content.appendChild(addToCartButton);
+
+  // Append the content container to the modal
+  modal.appendChild(content);
+  modal.appendChild(closeButton);
+
+  // Append the modal to the document body
+  document.body.appendChild(modal);
+}
+
+function closeModal(modal) {
+  // Remove the modal from the document body
+  document.body.removeChild(modal);
+}
+
+function addToCart(product) {
+  // Implement your "Add to Cart" logic here
+  console.log("Added to cart:", product);
 }
